8188bb613e084ba6abfd67feea8630d52db46583
sergeyblagodarnyy <sergey.blagodarnyy@faramax.com>
2023-02-15 12:10:52 +0100

    Added on_memory_concurrent_threads autotest

--- test/autotest_spsc_queue.cpp
+++ test/autotest_spsc_queue.cpp
@@ -214,56 +214,80 @@ BOOST_AUTO_TEST_SUITE(SPSCVarQueueSuite)
       }
    }
 
-   BOOST_AUTO_TEST_CASE(concurrent_threads)
-   {
+   struct concurrent_fixture {
       sce size_t block_size = sizeof(Block);
       sce size_t queue_size = 1024 * block_size;
-      sce size_t check_len  = 32 * queue_size;
-      SPSCVarQueue<queue_size> queue;
+      sce size_t check_len = 32 * queue_size;
+      using      queueT    = SPSCVarQueue<queue_size>;
+      queueT&    queue;
 
-      std::thread sender_thread([&]()
-         {
-            std::minstd_rand message_engine(check_len);
-            std::minstd_rand length_engine(check_len);
-            sce size_t max_message_size = 256;
-            for (size_t i = 0; i < check_len;)
-            {
-               auto const message_size = length_engine() % max_message_size;
-               decltype(auto) p_val = queue.alloc(message_size);
-               while(p_val == nullptr)
-                  p_val = queue.alloc(message_size);
-               char* msg_begin = (char*) (p_val + 1);
-               for(size_t j = 0; j < message_size; ++j)
-               {
-                  *msg_begin = static_cast<char>(message_engine());
-                  ++msg_begin;
-               }
-               queue.push();
-               i += message_size;
-            }
-         }
-      );
+      concurrent_fixture(queueT& q)
+         : queue(q)
+      {}
 
-      std::thread receiver_thread([&]()
-         {
-            std::minstd_rand message_engine(check_len);
-            for (size_t i = 0; i < check_len;)
+      std::thread make_sender_thread() {
+         return std::thread([&]() {
+                               std::minstd_rand message_engine(check_len);
+                               std::minstd_rand length_engine(check_len);
+                               sce size_t max_message_size = 256;
+                               for (size_t i = 0; i < check_len;) {
+                                  auto const message_size = length_engine() % max_message_size;
+                                  decltype(auto) p_val = queue.alloc(message_size);
+                                  while (p_val == nullptr)
+                                     p_val = queue.alloc(message_size);
+                                  char *msg_begin = (char *) (p_val + 1);
+                                  for (size_t j = 0; j < message_size; ++j) {
+                                     *msg_begin = static_cast<char>(message_engine());
+                                     ++msg_begin;
+                                  }
+                                  queue.push();
+                                  i += message_size;
+                               }
+                            }
+         );
+      }
+      std::thread make_receiver_thread()
+      {
+         return std::thread([&]()
             {
-               MsgHeader*  p_val        = queue.front();
-               while(p_val == nullptr)
-                  p_val = queue.front();
-               char*       msg_begin    = (char*) (p_val + 1);
-               auto const  message_size = p_val->size - sizeof(MsgHeader);
-               for(size_t j = 0; j < message_size; ++j)
-               {
-                  BOOST_REQUIRE_EQUAL(*msg_begin, static_cast<char>(message_engine()));
-                  ++msg_begin;
+               std::minstd_rand message_engine(check_len);
+               for (size_t i = 0; i < check_len;) {
+                  MsgHeader *p_val = queue.front();
+                  while (p_val == nullptr)
+                     p_val = queue.front();
+                  char *msg_begin = (char *) (p_val + 1);
+                  auto const message_size = p_val->size - sizeof(MsgHeader);
+                  for (size_t j = 0; j < message_size; ++j) {
+                     BOOST_REQUIRE_EQUAL(*msg_begin, static_cast<char>(message_engine()));
+                     ++msg_begin;
+                  }
+                  queue.pop();
+                  i += message_size;
                }
-               queue.pop();
-               i += message_size;
             }
-         }
-      );
+         );
+      }
+   };
+
+   BOOST_AUTO_TEST_CASE(concurrent_threads)
+   {
+      concurrent_fixture::queueT queue;
+      concurrent_fixture f{queue};
+      std::thread sender_thread = f.make_sender_thread();
+      std::thread receiver_thread = f.make_receiver_thread();
+      sender_thread.join();
+      receiver_thread.join();
+   }
+
+   BOOST_AUTO_TEST_CASE(on_memory_concurrent_threads)
+   {
+      using                queueT = concurrent_fixture::queueT;
+      size_t               memory_size = sizeof(queueT);
+      void*                memory_blob = malloc(memory_size);
+      queueT*              queue = new(memory_blob) queueT;
+      concurrent_fixture   f{*queue};
+      std::thread          sender_thread = f.make_sender_thread();
+      std::thread          receiver_thread = f.make_receiver_thread();
       sender_thread.join();
       receiver_thread.join();
    }
